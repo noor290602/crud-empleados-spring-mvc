d2291b11518b585b2f9a546102a61b4b9e656e5d
Noor Aloune Sagouma <nalosag@gmail.com>
2025-12-03 11:50:06 +0100

    Estado inicial del proyecto

---
+++ src/main/java/com/example/crud_empleados_spring_mvc/CrudEmpleadosSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.crud_empleados_spring_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudEmpleadosSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudEmpleadosSpringMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/crud_empleados_spring_mvc/CrudEmpleadosSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crud_empleados_spring_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudEmpleadosSpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
